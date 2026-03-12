13013158310030caae88f6c21e3d444c0751a786
Loome28 <iliescucorneliu1@gmail.com>
2026-03-13 01:49:55 +0200

    more practice

---
+++ sets_practice.py
@@ -0,0 +1,4 @@
+logins = ["ana", "bogdan", "ana", "cristina", "bogdan", "bogdan", "maria"]
+unique = set(logins)    
+print(f"Unique users: {unique}")
+print(f"Number of unique users: {len(unique)}")
---
+++ tuple_practice.py
@@ -0,0 +1,3 @@
+server = ("server_name", "192.168.1.1", 8080)
+for element in server:
+    print(element)
